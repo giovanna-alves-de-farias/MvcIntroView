a2217e858c6fbea0066bf98f1509181dd3f0f153
Giovanna Alves de Farias <101728198+giovanna-alves-de-farias@users.noreply.github.com>
2022-09-25 21:34:59 -0300

    Operações de subtração, multiplicação e divisão

--- MvcIntroViews/Controllers/OperacoesController.cs
+++ MvcIntroViews/Controllers/OperacoesController.cs
@@ -17,4 +17,34 @@ public class OperacoesController : Controller
         
         return View();
     }
+    
+    public IActionResult Subtracao() => View();
+
+    public IActionResult ResultadoSubtracao(double n1, double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 - n2;
+        return View();
+    }
+
+    public IActionResult Multiplicacao() => View();
+
+    public IActionResult ResultadoMultiplicacao(double n1, double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 * n2;
+        return View();
+    }
+
+    public IActionResult Divisao() => View();
+
+    public IActionResult ResultadoDivisao(double n1, double n2)
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 / n2;
+        return View();
+    }
 }
